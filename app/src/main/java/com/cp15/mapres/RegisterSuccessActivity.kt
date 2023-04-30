fd0d570414ff5cb221a637c133361ce27be644b2
Fangki Igo Pramana <fangkiigo26@gmail.com>
2023-04-30 21:06:43 +0700

    update UI Login, Register, and Welcome

---
+++ app/src/main/java/com/cp15/mapres/RegisterSuccessActivity.kt
@@ -0,0 +1,11 @@
+package com.cp15.mapres
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterSuccessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_success)
+    }
+}
---
+++ app/src/main/java/com/cp15/mapres/activity/WelcomeActivity.kt
@@ -0,0 +1,11 @@
+package com.cp15.mapres.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
